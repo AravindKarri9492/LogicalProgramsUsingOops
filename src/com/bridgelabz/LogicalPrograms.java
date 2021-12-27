516c9d8a9add142b7e75a19272c01e384a1c1a5f
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-27 22:27:35 +0530

    ADDED: check given number is prime or not

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -29,4 +29,20 @@ public class LogicalPrograms {
             System.out.println("it is not a perfect number");
         }
     }
+    //Prime Number
+    public void primeNumber(int number) {
+        boolean flag = true;
+        for(int i =2; i<number/2; i++) {
+            if(number % i == 0)
+            {
+                flag = false;
+            }
+        }
+        if(flag == true) {
+            System.out.println("It is a prime number");
+        }
+        else {
+            System.out.println("It is not a prime number");
+        }
+    }
 }
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,26 +6,35 @@ public class Main {
     public static void main(String[] args) {
         LogicalPrograms object = new LogicalPrograms();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter which program do you want to run :: ");
-        System.out.println("1.fibonacci \t" +
-                           "2.perfect number \t" +
-                            " 3.exit");
-        int userChoice = scanner.nextInt();
-        switch (userChoice) {
-            case 1:
-                System.out.println("Enter a number to print fibonacci series ");
-                int number = scanner.nextInt();
-                object.fibonacci(number);
-                break;
-            case 2:
-                System.out.println("Enter a number to check whether it is perfect number on not ::");
-                int num = scanner.nextInt();
-                object.perfectNumber(num);
-                break;
-            case 3:
-                System.exit(0);
-            default:
-                System.out.println("Choose correct option !");
+        while(true) {
+            System.out.println("Enter which program do you want to run :: ");
+            System.out.println("1.fibonacci \t" +
+                    "2.perfect number \t" +
+                    " 3.prime number" +
+                    "4.exit");
+            int userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Enter a number to print fibonacci series ");
+                    int number = scanner.nextInt();
+                    object.fibonacci(number);
+                    break;
+                case 2:
+                    System.out.println("Enter a number to check whether it is perfect number on not ::");
+                    int num = scanner.nextInt();
+                    object.perfectNumber(num);
+                    break;
+                case 3:
+                    System.out.println("Enter a number to check whether it is prime or not");
+                    int number1 = scanner.nextInt();
+                    object.primeNumber(number1);
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Choose correct option !");
+            }
+
         }
 
     }
