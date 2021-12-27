7ed60e81c397c71359ff6e1c43c2058ebda470de
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-27 21:50:00 +0530

    ADDED: fibonacci series logic developed

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -1,5 +1,17 @@
 package com.bridgelabz;
 
 public class LogicalPrograms {
-
+    //Fibonacci
+    public void fibonacci(int number){
+        int previousNum = 0;
+        int currentNum = 1;
+        System.out.print(previousNum + " " + currentNum);
+        int nextNum;
+        for(int count =2; count < number;count++) {
+            nextNum = previousNum + currentNum;
+            previousNum = currentNum;
+            currentNum = nextNum;
+            System.out.print(" " + nextNum);
+        }
+    }
 }
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,5 +1,14 @@
 package com.bridgelabz;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        LogicalPrograms object = new LogicalPrograms();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to print fibonacci series ");
+        int number = scanner.nextInt();
+        scanner.close();
+        object.fibonacci(number);
+    }
 }
