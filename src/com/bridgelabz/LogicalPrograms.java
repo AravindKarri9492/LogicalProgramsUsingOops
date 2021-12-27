a2c1b7252a15788479962e6c9dd86e0084c58543
AravindKarri9492 <n140035@rguktn.ac.in>
2021-12-27 22:10:25 +0530

    ADDED: perfect number logic developed

--- src/com/bridgelabz/LogicalPrograms.java
+++ src/com/bridgelabz/LogicalPrograms.java
@@ -14,4 +14,19 @@ public class LogicalPrograms {
             System.out.print(" " + nextNum);
         }
     }
+    //Perfect Number
+    public void perfectNumber(int number){
+        int factorsSum=0;
+        for(int i=1; i<=number/2; i++) {
+            if (number%i == 0) {
+                factorsSum += i;
+            }
+        }
+        if (factorsSum == number) {
+            System.out.println("It is a perfect number");
+        }
+        else {
+            System.out.println("it is not a perfect number");
+        }
+    }
 }
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,9 +6,27 @@ public class Main {
     public static void main(String[] args) {
         LogicalPrograms object = new LogicalPrograms();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number to print fibonacci series ");
-        int number = scanner.nextInt();
-        scanner.close();
-        object.fibonacci(number);
+        System.out.println("Enter which program do you want to run :: ");
+        System.out.println("1.fibonacci \t" +
+                           "2.perfect number \t" +
+                            " 3.exit");
+        int userChoice = scanner.nextInt();
+        switch (userChoice) {
+            case 1:
+                System.out.println("Enter a number to print fibonacci series ");
+                int number = scanner.nextInt();
+                object.fibonacci(number);
+                break;
+            case 2:
+                System.out.println("Enter a number to check whether it is perfect number on not ::");
+                int num = scanner.nextInt();
+                object.perfectNumber(num);
+                break;
+            case 3:
+                System.exit(0);
+            default:
+                System.out.println("Choose correct option !");
+        }
+
     }
 }
